f4a3a8147303b65c4535e8e00d9fc7f52a938ff7
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-17 16:23:50 +0200

    withES6

--- snack4/script.js
+++ snack4/script.js
@@ -1,36 +1,41 @@
-var table = '';
-var htmlTable = document.getElementById('table');
-var students = [
+let students = [
     { name: 'Pippo', surname: 'Baudo', age: 80, description: '' },
     { name: 'Otto', surname: 'Bisotto', age: 70, description: '' },
     { name: 'Paolino', surname: 'Paperino', age: 80, description: '' }
 ];
 
-for (i = 0; i < students.length; i++) {
-    var nameStudent = students[i].name;
+for (let i = 0; i < students.length; i++) {
+    let nameStudent = students[i].name;
     students[i].description = prompt('Aggiungi la descrizione dello studente ' + nameStudent);
 };
 
 console.log(students);
 
-table += "<thead><tr>";
-for (var key in students[0]) {
-    table += "<th>" + key.toUpperCase() + "</th>";
+//-----------------------------------
+
+const tableHTML = document.querySelector('.table-container');
+
+let thead = `<ul>`;
+for (let key in students[0]) {
+    thead += `<li>${key.toUpperCase()}</li>`;
 }
-table += "</tr></thead>";
+thead += `</ul>`;
+tableHTML.innerHTML += thead;
 
-table += "<tbody>";
-for (var i = 0; i < students.length; i++) {
-    table += "<tr>";
-    for (var key in students[i]) {
-        table += "<td>" + students[i][key] + "</td>";
-    }
-    table += "</tr>";
+
+for (let x = 0; x < students.length; x++) {
+
+    const { name, surname, age, description } = students[x];
+
+    tableHTML.innerHTML += `
+        <ul>
+            <li>${name}</li>
+            <li>${surname}</li>
+            <li>${age}</li>
+            <li>${description}</li>
+       </ul>
+    `
 };
-table += "</tbody>";
-
-
-htmlTable.innerHTML = table;
 
 
 
